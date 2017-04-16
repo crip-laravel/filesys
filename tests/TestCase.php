3b287f7427b62d819c00e110355c1977639c84ea
Igors Krasjukovs <tahq69@gmail.com>
2017-04-16 15:53:37 +0300

    configuring tests for back-end code;

---
+++ tests/TestCase.php
@@ -0,0 +1,65 @@
+<?php namespace Crip\Filesys;
+
+use Crip\Core\Support\PackageBase;
+use League\Flysystem\Vfs\VfsAdapter;
+use VirtualFileSystem\FileSystem as Vfs;
+use League\Flysystem\Filesystem;
+
+/**
+ * Class TestCase
+ * @package Crip\Filesys
+ */
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var PackageBase
+     */
+    protected $package;
+
+    /**
+     * Setup the test environment.
+     * @return void
+     */
+    public function setUp()
+    {
+        $vfs = new Vfs();
+        $adapter = new VfsAdapter($vfs);
+
+        $this->filesystem = new Filesystem($adapter);
+        $this->package = new PackageBase('cripfilesys', '');
+
+        parent::setUp();
+    }
+
+    /**
+     * Define environment setup.
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('filesystems.default', 'vfs');
+        $app['config']->set('filesystems.disks.vfs', [
+            'driver' => 'vfs'
+        ]);
+
+        $app['filesystem']->extend('vfs', function () {
+            return $this->filesystem;
+        });
+    }
+
+    /**
+     * Get package providers.
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [CripFilesysServiceProvider::class];
+    }
+}
